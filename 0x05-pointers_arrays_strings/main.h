51bb1efb634bbcc859b0f8733975eb560e11b525
PhoenixClix <mfirstborn@gmail.com>
2022-10-26 15:42:23 +0000

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 /*int _putchar(char c);*/
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
 #endif 

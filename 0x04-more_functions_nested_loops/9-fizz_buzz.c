83dcb80d116d194e5f12203254135ddefffe0916
PhoenixClix <mfirstborn@gmail.com>
2022-11-05 21:04:01 +0000

    Program that prints the numbers from 1 to 100, Fizz, Buzz, FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 /**
-  * main - Entry point
+  * main - Prints the numbers from 1-100, but Fizz for multiples of three,
+  *        Buzz for multiples of five,
+  *        and FizzBuzz for multiples of both three and five.
   *
   * Return: Always 0
   */

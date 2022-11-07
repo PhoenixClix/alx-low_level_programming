dfd5c9b9b506f8f5450c863d2b1e2f2c296d12cf
PhoenixClix <mfirstborn@gmail.com>
2022-11-07 01:41:29 +0000

    Program that prints the minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -36,22 +36,18 @@ int main(int argc, char *argv[])
 			cents -= 10;
 			continue;
 		}
-
 		if ((cents - 5) >= 0)
 		{
 			cents -= 5;
 			continue;
 		}
-
 		if ((cents - 2) >= 0)
 		{
 			cents -= 2;
 			continue;
 		}
-
 		cents--;
 	}
-
 	printf("%d\n", coins);
 	return (0);
 }
